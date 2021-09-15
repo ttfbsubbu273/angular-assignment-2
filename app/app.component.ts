76806316d98beea5268c339b54f3ce7ad0e3f3b7
Subrahmanya Bhat <56573851+ttfbsubbu273@users.noreply.github.com>
2021-09-16 00:06:05 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'formapp';
+
+  userModule = new User('Harry', 'harry@gmail.com',8899009900,'Bangalore','23/09/1996')
+}
---
+++ app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public empname: string,
+        public emailid: string,
+        public phone: number,
+        public address: string,
+        public dob: string
+    ){}
+}
